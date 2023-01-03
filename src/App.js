1d04dd36625dc6fca54f99b057c429f1278b7778
fahreza raharjo <fahrezaraharjo92@gmail.com>
2023-01-03 16:41:12 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import './App.scss';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-     App
+import { Footer, Blog, Possibility, Features, WhatGPT3, Header } from './containers';
+import { CTA, Brand, Navbar } from './components';
+
+import './App.css';
+
+const App = () => (
+  <div className="App">
+    <div className="gradient__bg">
+      <Navbar />
+      <Header />
     </div>
-  );
-}
+    <Brand />
+    <WhatGPT3 />
+    <Features />
+    <Possibility />
+    <CTA />
+    <Blog />
+    <Footer />
+  </div>
+);
 
 export default App;
